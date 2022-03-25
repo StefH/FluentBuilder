97cf14bdee1db9c246f1922a6b541b467aab7146
Stef Heyenrath <stef.heyenrath@gmail.com>
2022-03-25 20:00:23 +0100

    .

--- src-examples/BuilderConsumerFileScopedNamespace/Program.cs
+++ src-examples/BuilderConsumerFileScopedNamespace/Program.cs
@@ -1,6 +1,7 @@
 var user = new UserBuilder()
     .WithFirstName("Test")
     .WithLastName("User")
+    .WithDate(DateTime.MaxValue)
     .Build();
 
 Console.WriteLine($"{user.FirstName} {user.LastName}");
