f0beacba92f013885f7a24da3b423e37f0bfdc98
Henry <emorell96@hotmail.com>
2022-06-30 23:14:50 +0200

    made the attribute internal and sealed. Ran the tests so that the autogenerator would inject the IgnoreProperty attribute into the tests and I could comment out the "cheat" attribute in SimpleClass.cs I also packed a dev version and installed it on my own current project and the ignore attribute works as expected.

--- tests/FluentBuilderGeneratorTests/DTO/SimpleClass.cs
+++ tests/FluentBuilderGeneratorTests/DTO/SimpleClass.cs
@@ -6,17 +6,17 @@ namespace FluentBuilderGeneratorTests.DTO
     public class SimpleClass
     {
         public int Id { get; set; }
-        [IgnoreProperty] //this should be ignored or it will fail.
+        [IgnoreProperty] //this property should be ignored or it will fail.
         public System.Globalization.CultureInfo? CultureInfo { get; set; }
     }
 }
-namespace FluentBuilder
-{
-    [System.AttributeUsage(AttributeTargets.Property)]
-    public class IgnorePropertyAttribute : Attribute
-    {
-    }
-}
+//namespace FluentBuilder
+//{
+//    [System.AttributeUsage(AttributeTargets.Property)]
+//    public class IgnorePropertyAttribute : Attribute
+//    {
+//    }
+//}
 
 
 
