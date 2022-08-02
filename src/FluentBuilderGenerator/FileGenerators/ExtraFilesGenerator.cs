726ac984cc08fa08aac6a1e7f35b859865ed8338
Stef Heyenrath <stef.heyenrath@gmail.com>
2022-08-02 21:30:48 +0200

    v1

--- src/FluentBuilderGenerator/FileGenerators/ExtraFilesGenerator.cs
+++ src/FluentBuilderGenerator/FileGenerators/ExtraFilesGenerator.cs
@@ -75,20 +75,12 @@ namespace FluentBuilder
     {{
     }}
 
-    // Based on Microsoft.CodeAnalysis.Accessibility
     [Flags]
     internal enum FluentBuilderAccessibility
     {{
         All = 0,
         Private = 1,
-        // ProtectedAndInternal = 2,
-        // ProtectedAndFriend = ProtectedAndInternal,
-        // Protected = 3,
-        // Internal = 4,
-        // Friend = Internal,
-        // ProtectedOrInternal = 5,
-        // ProtectedOrFriend = ProtectedOrInternal,
-        Public = 6
+        Public = 2
     }}
 }}
 {(_supportsNullable ? "#nullable disable" : string.Empty)}"
--- src/FluentBuilderGenerator/Types/FluentBuilderAccessibility.cs
+++ src/FluentBuilderGenerator/Types/FluentBuilderAccessibility.cs
@@ -5,12 +5,5 @@ internal enum FluentBuilderAccessibility
 {
     All = 0,
     Private = 1,
-    // ProtectedAndInternal = 2,
-    // ProtectedAndFriend = ProtectedAndInternal,
-    // Protected = 3,
-    // Internal = 4,
-    // Friend = Internal,
-    // ProtectedOrInternal = 5,
-    // ProtectedOrFriend = ProtectedOrInternal,
-    Public = 6
+    Public = 2
 }
