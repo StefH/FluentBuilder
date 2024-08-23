1b51f45c3f50765d1b92a06929eec825adf5b8ee
Stef Heyenrath <stef.heyenrath@gmail.com>
2024-08-23 21:23:42 +0200

    .

--- src-examples/BuilderConsumer/Settings.cs
+++ src-examples/BuilderConsumer/Settings.cs
@@ -5,7 +5,5 @@ namespace BuilderConsumer
     public class Settings
     {
         public IReadOnlyCollection<Content> Contents { get; set; }
-
-        public IReadOnlyCollection<int> X { get; set; } 
     }
 }
