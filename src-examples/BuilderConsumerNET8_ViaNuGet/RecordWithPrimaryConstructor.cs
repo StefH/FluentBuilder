38efbfdcc2ee2544f81de6c4fab6b132e8d2037d
Stef Heyenrath <Stef.Heyenrath@gmail.com>
2025-08-31 10:48:59 +0200

    Revert Microsoft.CodeAnalysis.CSharp to version 4.8.0 (#73)
    * Revert Microsoft.CodeAnalysis.CSharp to version 4.13.0
    
    * ubuntu-latest
    
    * .NET 6 SDK
    
    * 411
    
    * no 6
    
    * no 6 / no 8
    
    * Include="Microsoft.CodeAnalysis.CSharp" Version="4.8.0"
    
    * .
    
    * 092
    
    * .
    
    * 8
    
    * version -> 013

--- src-examples/BuilderConsumerNET8_ViaNuGet/RecordWithPrimaryConstructor.cs
+++ src-examples/BuilderConsumerNET8_ViaNuGet/RecordWithPrimaryConstructor.cs
@@ -1,11 +1,11 @@
-using FluentBuilder;
+//using FluentBuilder;
 
-namespace BuilderConsumerNET8;
+//namespace BuilderConsumerNET8;
 
-[AutoGenerateBuilder]
-public record RecordWithPrimaryConstructor(string Test, int Num)
-{
-    public string Normal { get; set; } = string.Empty;
+//[AutoGenerateBuilder]
+//public record RecordWithPrimaryConstructor(string Test, int Num)
+//{
+//    public string Normal { get; set; } = string.Empty;
 
-    public string Data { get; } = Test + Num;
-}
+//    public string Data { get; } = Test + Num;
+//}
